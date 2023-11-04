cf4dfeea6cc375d167fb355e475b1903822023c5
Pavel <magma4810@gmail.com>
2023-11-04 17:21:19 +0300

    create fetch weather

--- weather.js
+++ weather.js
@@ -2,6 +2,13 @@
     function addToList(list,items){
         list.innerHTML = `<ol>${items.map((el)=>`<li>${el}</li>`).join('')}</ol>`
     }
+    async function getWeather(city,items,list){
+        const key = "ab4639f5754271e773ed6d3ffd73f327";
+        const reject = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${key}`)
+        const weather = await reject.json();
+        await items.push(weather.main.temp);
+        addToList(list,items)
+    }
     const items = [];
     //faddgaefw
     const form = document.querySelector('form')
@@ -12,8 +19,9 @@
         const input = formElement.querySelector('input');
         const value = input.value;
         input.value = '';
-        items.push(value)
+        
         const list = formElement.querySelector('div');
-        addToList(list,items)
+        getWeather(value,items,list)
+        
     })
 })();
